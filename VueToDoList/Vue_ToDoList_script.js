8656edd97193267f1708416a4b44e0d7c5a52c45
Baklanozavr <abaklan88@gmail.com>
2019-05-20 07:34:58 +0700

    Внёс исправления: 1. Добавил подключение jQuery. 2. Добавил стили валидации. 3. Изменил функции изменения/удаления заметки. 4. Удалил лишний вывод в консоль. 5. Добавил в репозиторий package-lock.json.

--- VueToDoList/Vue_ToDoList_script.js
+++ VueToDoList/Vue_ToDoList_script.js
@@ -4,7 +4,8 @@ Vue.component("note-li", {
         return {
             draftNote: this.note.text,
             isEditNow: false,
-            isReadyToConfirm: true
+            isReadyToConfirm: true,
+            isInputDataInvalid: false
         };
     },
     template: "#note-tr-template",
@@ -12,19 +13,24 @@ Vue.component("note-li", {
         cancelChanges: function () {
             this.draftNote = this.note.text;
             this.isEditNow = false;
+            this.isInputDataInvalid = false;
         },
         cancelChangesIfPossible: function () {
-            if(!this.isReadyToConfirm) {
-                console.log("blur");
+            if (!this.isReadyToConfirm) {
                 this.cancelChanges();
             }
         },
         changeThisNote: function () {
-            this.$emit("change-note-by-id", this.note.id, this.draftNote);
+            if (this.draftNote === "") {
+                this.isInputDataInvalid = true;
+                this.$refs.draftInput.focus();
+                return;
+            }
+            this.$emit("change-note", this.note, this.draftNote);
             this.cancelChanges();
         },
         deleteThisNote: function () {
-            this.$emit("remove-note-by-id", this.note.id);
+            this.$emit("remove-note", this.note);
         },
         editThisNote: function () {
             this.draftNote = this.note.text;
@@ -42,12 +48,14 @@ new Vue({
     el: "#noteList",
     data: {
         inputText: "",
+        isInputDataInvalid: false,
         notes: [],
         counter: 0
     },
     methods: {
         addNote: function () {
             if (this.inputText === "") {
+                this.isInputDataInvalid = true;
                 return;
             }
             this.notes.push({
@@ -58,117 +66,17 @@ new Vue({
             ++this.counter;
             this.$emit("blur");
         },
-        changeNoteById: function (id, draft) {
-            this.notes = this.notes.map(function (element) {
-                if (id === element.id) {
-                    element.text = draft;
-                }
-                return element;
-            })
+        changeNote: function (note, draft) {
+            var indexOfInputNote = this.notes.indexOf(note);
+            this.notes[indexOfInputNote].text = draft;
         },
-        removeNoteById: function (id) {
+        cleanInputClass: function () {
+            this.isInputDataInvalid = false;
+        },
+        removeNote: function (note) {
             this.notes = this.notes.filter(function (element) {
-                return element.id !== id;
+                return element.id !== note.id;
             })
         }
     }
 });
-//
-// $(function () {
-//     var inputTextField = $(".new-text")
-//         .focus(function () {
-//             inputTextField.removeClass("invalid");
-//         });
-//
-//     var list = $(".list");
-//
-//     var createButton = $(".create-button");
-//     createButton.click(function () {
-//         var text = inputTextField.val();
-//
-//         if (text === "") {
-//             inputTextField.addClass("invalid");
-//             return;
-//         }
-//         inputTextField.val("");
-//
-//         var newNoteRow = $("<tr></tr>")
-//             .appendTo(list);
-//
-//         var textArea = $("<td></td>")
-//             .addClass("note")
-//             .appendTo(newNoteRow);
-//
-//         var noteText = $("<span></span>")
-//             .text(text)
-//             .appendTo(textArea);
-//
-//         var editArea = $("<input>")
-//             .addClass("edit-text")
-//             .val(text)
-//             .appendTo(textArea);
-//
-//         var firstButtonArea = $("<td></td>")
-//             .appendTo(newNoteRow);
-//         var editButton = $("<button>Edit</button>")
-//             .addClass("usual-button")
-//             .appendTo(firstButtonArea);
-//         var confirmButton = $("<button>Confirm</button>")
-//             .addClass("usual-button")
-//             .appendTo(firstButtonArea);
-//
-//         var secondButtonArea = $("<td></td>")
-//             .appendTo(newNoteRow);
-//         var deleteButton = $("<button>Delete</button>")
-//             .addClass("usual-button")
-//             .appendTo(secondButtonArea);
-//         var cancelButton = $("<button>Cancel</button>")
-//             .addClass("usual-button")
-//             .appendTo(secondButtonArea);
-//
-//         switchEditOff();
-//
-//         editButton.click(function () {
-//             text = noteText.text();
-//             noteText.text("");
-//             editArea.val(text);
-//             switchEditOn();
-//         });
-//
-//         confirmButton.mouseleave(function () {
-//             editArea.blur(mouseOutOfEditButtonListener);
-//         });
-//         confirmButton.mouseenter(function () {
-//             editArea.off("blur", mouseOutOfEditButtonListener);
-//         });
-//         confirmButton.click(function () {
-//             noteText.text(editArea.val());
-//             switchEditOff();
-//         });
-//
-//         deleteButton.click(function () {
-//             newNoteRow.remove();
-//         });
-//
-//         var mouseOutOfEditButtonListener = function () {
-//             noteText.text(text);
-//             switchEditOff();
-//         };
-//
-//         function switchEditOn() {
-//             editArea.show().focus();
-//             confirmButton.show();
-//             cancelButton.show();
-//             editButton.hide();
-//             deleteButton.hide();
-//         }
-//
-//         function switchEditOff() {
-//             editArea.hide();
-//             confirmButton.hide();
-//             cancelButton.hide();
-//             editButton.show();
-//             deleteButton.show();
-//         }
-//     });
-// });
